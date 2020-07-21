e2a12df87c2609c026274a809b640af251882418
Milan Panchal <sam07it22@gmail.com>
2020-07-21 23:28:03 +0530

    Initial Commit

---
+++ SwiftLint Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLint Demo
+//
+//  Created by Milan Panchal on 21/07/20.
+//  Copyright © 2020 Jeenal Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
