c21b511f8755d2af6aaa084c7747ed8be8adea60
Milan Panchal <sam07it22@gmail.com>
2020-08-30 00:42:21 +0530

    Add swiftlint.yml configuration file

--- SwiftLint Demo/ViewController.swift
+++ SwiftLint Demo/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.random
+        printOddNumbers(number: 10)
     }
 
+    private func printOddNumbers(number:Int) {
+        for i in 1...number {
+            if i%2 == 1 {
+                print(i)
+            }
+        }
+    }
 
+    
+}
+
+extension UIColor {
+    static var random:UIColor {
+        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
 }
 
